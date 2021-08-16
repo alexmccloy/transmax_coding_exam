df950743d44ab6ea73208f27202916c857df93f0
Alex McCloy <alex.mccloy@qr.com.au>
2021-08-16 18:29:51 +1000

    Completed EagleBot Simulator

--- EagleBotSimulator/Program.cs
+++ EagleBotSimulator/Program.cs
@@ -1,12 +1,73 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace EagleBotSimulator
 {
     class Program
     {
-        static void Main(string[] args)
+        private static HttpClient _httpClient = new HttpClient();
+        private static Random _random = new Random();
+        
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var count = int.Parse(Environment.GetEnvironmentVariable("EagleBotSimulator_Count") ?? "5");
+            var tasks = new List<Task>();
+
+            // Create tasks to loop and send random data forever
+            for (int i = 0; i < count; i++)
+            {
+                int i1 = i;
+                tasks.Add(Task.Run(async () =>
+                {
+                    string id = $"eaglebot-{i1}";
+                    while (true)
+                    {
+                        // Send random data forever, and wait 5-15 secs between each
+                        await SendRandomData(id);
+                        await Task.Delay(_random.Next(5000, 15000));
+                    }
+                }));
+            }
+
+            // Wait forever
+            await Task.WhenAll(tasks);
         }
+
+        private static async Task SendRandomData(string id)
+        {
+            try
+            {
+                var stringContent = new StringContent(CreateRandomData(id), Encoding.Default, "application/json");
+                // await _httpClient.PostAsync("http://localhost:5000/api/eaglebot", stringContent);
+                await _httpClient.PostAsync("http://eaglerock:80/api/eaglebot", stringContent);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Failed to write to {id}: {e.Message}");
+            }
+        }
+
+        private static string CreateRandomData(string id)
+        {
+            return JsonConvert.SerializeObject(new {
+                eagleBotId= id,
+                timestamp= DateTime.Now,
+                coordinates= new {
+                    latitude= GetRandomDouble(-90,90),
+                    longitude= GetRandomDouble(-180,180)
+                },
+                streetName= "Kingsford Smith Drive",
+                trafficDirection= "Inbound",
+                trafficFlowRate= GetRandomDouble(0, 1000),
+                averageVehicleSpeed= GetRandomDouble(0, 200)
+            });
+        }
+        
+        private static double GetRandomDouble(double minimum, double maximum) => 
+            _random.NextDouble() * (maximum - minimum) + minimum;
     }
 }
---
+++ EagleRock/Controllers/OperatorController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using EagleRock.Cache;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+
+namespace EagleRock.Controllers
+{
+    /// <summary>
+    /// Interface for other backend systems to view EagleBot data
+    /// </summary>
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OperatorController : ControllerBase
+    {
+        private readonly ILogger<OperatorController> _logger;
+        private readonly ICacheInterface _cacheInterface;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="logger">Logging interface</param>
+        /// <param name="cacheInterface">Interface to cache the most recent EagleBot payload</param>
+        public OperatorController(ILogger<OperatorController> logger, ICacheInterface cacheInterface)
+        {
+            _logger = logger;
+            _cacheInterface = cacheInterface;
+        }
+
+        /// <summary>
+        /// Gets the current dataset for a single EagleBot. If the EagleBot Id is null then the data for all EagleBots
+        /// will be returned instead.
+        /// </summary>
+        /// <param name="eagleBotId">The Id of the EagleBot to retrieve data for, or null for all data</param>
+        [HttpGet]
+        public async Task<IActionResult> Get(string eagleBotId = null)
+        {
+            try
+            {
+                if (eagleBotId is null)
+                {
+                    return new OkObjectResult(await _cacheInterface.GetAllPayloads());
+                }
+                else
+                {
+                    return new OkObjectResult(await _cacheInterface.GetPayload(eagleBotId));
+                }
+            }
+            catch (RedisConnectionException e)
+            {
+                _logger.LogError("Failed to retrieve EagleBot payload - a connection to the cache could not be established", e);
+                return StatusCode(StatusCodes.Status503ServiceUnavailable);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"An unexpected error occured while trying to retrieve an EagleBot payload with Id {eagleBotId}", e);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
