6de7260d608fcdfae016cb19aa9de98c348d0d7a
Alex McCloy <alex.mccloy@qr.com.au>
2021-08-17 22:13:31 +1000

    Cleaned up comments

--- EagleRock/Amqp/IAmqpInterface.cs
+++ EagleRock/Amqp/IAmqpInterface.cs
@@ -2,8 +2,15 @@
 
 namespace EagleRock.Amqp
 {
+    /// <summary>
+    /// Interface to publish data to AMQP
+    /// </summary>
     public interface IAmqpInterface
     {
+        /// <summary>
+        /// Publish an EagleBot payload to AMQP
+        /// </summary>
+        /// <param name="payload"></param>
         void PublishEvent(EagleBotPayload payload);
     }
 }
--- EagleRock/Cache/ICacheInterface.cs
+++ EagleRock/Cache/ICacheInterface.cs
@@ -4,10 +4,28 @@ using EagleRock.Model;
 
 namespace EagleRock.Cache
 {
+    /// <summary>
+    /// Interface to store and load values from a cache
+    /// </summary>
     public interface ICacheInterface
     {
+        /// <summary>
+        /// Stores an EagleBot Payload into the cache. If this value already exists it will be overwritten
+        /// </summary>
+        /// <param name="payload">The data to store in cache</param>
         Task StorePayload(EagleBotPayload payload);
+        
+        /// <summary>
+        /// Retrieves a payload from the cache matching the given id
+        /// </summary>
+        /// <param name="id">The id of the <see cref="EagleBotPayload"/> to retrieve</param>
+        /// <returns>An <see cref="EagleBotPayload"/> with the matching id</returns>
         Task<EagleBotPayload> GetPayload(string id);
+        
+        /// <summary>
+        /// Retrieves all payloads from the cache that contain <see cref="EagleBotPayload"/>s
+        /// </summary>
+        /// <returns>All the <see cref="EagleBotPayload"/>s that are available in the cache</returns>
         Task<IEnumerable<EagleBotPayload>> GetAllPayloads();
     }
 }
